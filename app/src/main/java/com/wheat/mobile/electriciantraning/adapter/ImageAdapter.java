4532c4cd8c73bd365348c2a63454415efc807963
mhhwheat <1057251411@qq.com>
2016-09-05 18:43:00 +0800

    add ImageAdapter

--- app/src/main/java/com/wheat/mobile/electriciantraning/adapter/ImageAdapter.java
+++ app/src/main/java/com/wheat/mobile/electriciantraning/adapter/ImageAdapter.java
@@ -1,7 +1,14 @@
 package com.wheat.mobile.electriciantraning.adapter;
 
+import android.net.Uri;
+import android.text.TextUtils;
+import android.util.Log;
 import android.widget.ImageView;
 
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.Picasso;
+import com.taobao.weex.WXEnvironment;
+import com.taobao.weex.WXSDKManager;
 import com.taobao.weex.adapter.IWXImgLoaderAdapter;
 import com.taobao.weex.common.WXImageStrategy;
 import com.taobao.weex.dom.WXImageQuality;
@@ -11,7 +18,61 @@ import com.taobao.weex.dom.WXImageQuality;
  */
 public class ImageAdapter implements IWXImgLoaderAdapter {
     @Override
-    public void setImage(String url, ImageView view, WXImageQuality quality, WXImageStrategy strategy) {
+    public void setImage(final String url, final ImageView view, WXImageQuality quality, final WXImageStrategy strategy) {
+        Log.i("ImageAdapter","setImage:"+url);
+        WXSDKManager.getInstance().postOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                if(view==null||view.getLayoutParams()==null){
+                    return;
+                }
 
+                if(TextUtils.isEmpty(url)){
+                    view.setImageBitmap(null);
+                    return;
+                }
+
+                String temp=null;
+                if(url.startsWith("//")){
+                    temp="http:"+url;
+                }
+
+                if(view.getLayoutParams().width<=0||view.getLayoutParams().height<=0){
+                    return;
+                }
+
+                if(!TextUtils.isEmpty(strategy.placeHolder)){
+                    Picasso.Builder builder=new Picasso.Builder(WXEnvironment.getApplication());
+                    Picasso picasso=builder.build();
+                    picasso.load(Uri.parse(strategy.placeHolder)).into(view);
+
+                    view.setTag(strategy.placeHolder.hashCode(),picasso);
+                }
+
+                Picasso.with(WXEnvironment.getApplication())
+                        .load(temp)
+                        .into(view, new Callback() {
+                            @Override
+                            public void onSuccess() {
+                                Log.i("load Image sucess","setImage:"+url);
+                                if(strategy.getImageListener()!=null){
+                                    strategy.getImageListener().onImageFinish(url,view,true,null);
+                                }
+
+                                if(!TextUtils.isEmpty(strategy.placeHolder)){
+                                    ((Picasso)view.getTag(strategy.placeHolder.hashCode())).cancelRequest(view);
+                                }
+                            }
+
+                            @Override
+                            public void onError() {
+                                Log.i("load Image error","setImage:"+url);
+                                if(strategy.getImageListener()!=null){
+                                    strategy.getImageListener().onImageFinish(url,view,false,null);
+                                }
+                            }
+                        });
+            }
+        },0);
     }
 }
